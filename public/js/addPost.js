d002d2daff863a25c00fab3f3fefc00460a80aa6
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-11-02 18:50:50 -0500

    added code to addPost.js

---
+++ public/js/addPost.js
@@ -0,0 +1,24 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const postTitle = document.querySelector('input[name="post-title"]').value;
+  const postContent = document.querySelector('input[name="content"]').value;
+
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({ postTitle, postContent }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to create blog");
+  }
+}
+
+document
+  .querySelector("#new-post-form")
+  .addEventListener("submit", newFormHandler);
