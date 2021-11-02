441fb235265160046db7e53136db8fe56d0f47c0
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-11-02 15:59:51 -0500

    renamed blogpost.js, added code to comment.js, and added code to post.js

--- models/Post.js
+++ models/Post.js
@@ -1,49 +1,44 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Comment extends Model {}
+class Post extends Model {}
 
-Comment.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    comment: {
+    title: {
       type: DataTypes.STRING,
-      validate: {
-        len: [1],
-      },
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: "user",
         key: "id",
       },
     },
     date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    blog_post_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: "post",
-        key: "id",
-      },
+      defaultValue: DataTypes.NOW,
     },
   },
   {
     sequelize,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: "comment",
+    modelName: "post",
   }
 );
 
-module.exports = Comment;
+module.exports = Post;
